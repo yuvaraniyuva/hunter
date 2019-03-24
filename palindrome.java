275a1fcf2a4a736cc01a1ef7788310297da12afc
yuvaraniyuva <40492725+yuvaraniyuva@users.noreply.github.com>
2019-03-24 21:42:40 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+import java.util.Scanner;
+class PalindromeTest {
+
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+        String inputString = in.nextLine();
+        Stack stack = new Stack();
+
+        for (int i = 0; i < inputString.length(); i++) {
+            stack.push(inputString.charAt(i));
+        }
+
+        String reverseString = "";
+
+        while (!stack.isEmpty()) {
+            reverseString = reverseString+stack.pop();
+        }
+
+        if (inputString.equals(reverseString))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+    }
+}
